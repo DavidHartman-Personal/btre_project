519551caf78de015e7de8f592d56c1ba5a4e79e8
David Hartman <dhartman@ptc.com>
2019-12-09 05:50:17 -0500

    Updated listing detail function call and HTML page to build dynamic content with listing details.

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import HttpResponse
 
@@ -46,7 +46,12 @@ def listing(request, listing_id):
     Returns:
         render: call to render with listings.html URL
     """
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+            'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
