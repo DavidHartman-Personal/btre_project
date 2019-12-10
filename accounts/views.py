0493d3d4a6aaba117f8078e6f2ce4a40cbd96678
David Hartman <dhartman@ptc.com>
2019-12-10 08:20:23 -0500

    Completed updates to login, logout, register and the dashboard.

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,44 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 
 def register(request):
     # If the request is a post then it is a form submission
     if request.method == 'POST':
         # Register user
-        print("Posted")
-        messages.error(request, 'Testing Error message')
-        return redirect('register')
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Confirm passwords match
+        if password == password2:
+            # Check is username already exists
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'User already exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already registered')
+                    return redirect('register')
+                else:
+                    #Validation successful, register user
+                    user = User.objects.create_user(username=username,
+                                                    password=password,
+                                                    email=email,
+                                                    first_name=first_name,
+                                                    last_name=last_name)
+                    user.save()
+                    # login after register
+                    auth.login(request, user)
+                    messages.success(request, 'User registered and now logged in')
+                    return redirect('dashboard')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
@@ -16,14 +46,25 @@ def register(request):
 def login(request):
     # If the request is a post then it is a form submission
     if request.method == 'POST':
-        # Register user
-        print("Login User")
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
+        return redirect('index')
 
 
 def dashboard(request):
